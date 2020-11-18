aad7c99f7586b059b199fe7aed36414445d4e63f
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-11-17 18:30:55 -0700

    Consolidate ignorable checks into single if statement

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const client = new Discord.Client()
 const prefix = '!!'
 client.on('message', message => {
     const isIgnorable = message.author.bot || !message.content.startsWith(prefix)
-    if (message.author.bot) return
-    if (!message.content.startsWith(prefix)) return
+    if (isIgnorable) return
 
     const commandBody = message.content.slice(prefix.length)
     const args = commandBody.split(' ')
@@ -14,7 +13,6 @@ client.on('message', message => {
 
     switch (command) {
         case 'ping':
-            const timeTaken = Date.now() - message.createdTimestamp
             message.reply(`Go fuck yourself!`)
             break;
 
