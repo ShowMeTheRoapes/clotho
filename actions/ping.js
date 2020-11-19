00cec91ff0aed34640b09cb0bc7149b099b91507
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-11-18 23:46:21 -0700

    Add JSDoc comment to ping function

--- actions/ping.js
+++ actions/ping.js
@@ -1,3 +1,7 @@
+/**
+ * A simple ping function to check if the bot is running.
+ * @param {object} message The Message object from the Discord.js API
+ */
 function ping(message) {
     message.reply('Clotho is up and running!')
 }
