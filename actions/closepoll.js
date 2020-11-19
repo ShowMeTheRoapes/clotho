ea933a6b1d5db6b5c022d8c83eec151a30d30f72
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-11-19 00:33:55 -0700

    Extract closepoll function to its own file

---
+++ actions/closepoll.js
@@ -0,0 +1,34 @@
+const { STRAWPOLL_KEY } = require('../config.json')
+
+/**
+ * Close the current poll, create a StrawPoll, and post it in the channel automatically.
+ * @param {object} poll The Poll object holding all Poll information
+ * @param {object} message The Message object from the Discord.js API
+ */
+async function closePoll(poll, message) {
+    if (!poll.title) {
+        message.reply('There is currently no active poll! Please begin a poll with the **!!startpoll** command (**!!help** for more details).')
+        return
+    }
+
+    if (!Object.keys(poll.candidates).length) {
+        message.reply('No candidates have been submitted yet! Please wait for all members to submit their candidates for this poll using the **!!submit** command before closing it (**!!help** for more details).')
+        return
+    }
+
+    message.channel.send('Thank you for your submissions! A poll will be created with your candidates shortly.')
+
+    const body = {
+        poll: {
+            title: poll.title,
+            answers: Object.values(poll.candidates).map(c => c.candidate),
+        }
+    }
+
+    const createResponse = await axios.post('https://strawpoll.com/api/poll', body, { headers: { 'API-KEY': STRAWPOLL_KEY } })
+    poll.strawPollId = createResponse.data.content_id
+
+    message.channel.send(`Please vote on StrawPoll: https://strawpoll.com/${poll.strawPollId}`)
+}
+
+module.exports = closePoll
--- actions/index.js
+++ actions/index.js
@@ -1,11 +1,13 @@
 const ping = require('./ping')
 const startPoll = require('./startpoll')
 const submit = require('./submit')
+const closePoll = require('./closepoll')
 const help = require('./help')
 
 module.exports = {
     ping,
     startPoll,
     submit,
+    closePoll,
     help,
 }
