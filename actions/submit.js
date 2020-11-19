119981c590f56c65d61c214fde48bf9c27532a5c
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-11-19 16:35:26 -0700

    Extract validation function for submitting candidates

--- actions/submit.js
+++ actions/submit.js
@@ -1,4 +1,30 @@
 const { Message } = require('discord.js')
+const { directMessage } = require('../helpers')
+
+/**
+ * Validate the input and the poll state before allowing another poll to start
+ * @param {object} poll The Poll object holding all Poll information
+ * @param {string} candidate The proposed candidate being submitted
+ */
+async function validate(poll, candidate) {
+    if (!poll.title) {
+        throw new Error('A poll has not been started! Please start one with the **!!startpoll** command.')
+    }
+
+    if (!candidate) {
+        throw new Error('No candidate was provided! Please use the **!!help** command to see proper usage')
+    }
+
+    const duplicateSubmissions = Object.keys(poll.candidates)
+        .filter(user => user !== author.username)
+        .filter(user => poll.candidates[user].candidate.toLowerCase() === candidate.toLowerCase())
+
+    if (duplicateSubmissions.length) {
+        await directMessage(`Unfortunately, your proposed candidate "${candidate}" has already been submitted. Please submit a different candidate.`)
+        throw new Error('Your candidate has already been submitted by another user. Please submit again!')
+    }
+}
+
 /**
  * Allow a user to submit a candidate for the poll.
  * If the user has already submitted a candidate and another is submitted,
@@ -8,35 +34,27 @@ const { Message } = require('discord.js')
  * @param {string[]} args The arguments that were provided after the command
  */
 async function submit(poll, message, args) {
-    const newCandidate = args.join(' ')
-    const { username } = message.author
+    const newCandidate = args.join(' ').trim()
+    const { author } = message
+    const { username } = author
 
-    if (username in poll.candidates) {
-        message.reply("Clotho has received another submission from you! It will replace your previous submission.")
-
-        const dm = await message.author.createDM()
-        dm.send(`Clotho received another submission from you!\nYour previous candidate, "${poll.candidates[username].candidate}", will be replaced with your new submission, "${newCandidate}".`)
-    } else {
-        message.reply('your submission has been saved!')
-
-        const dm = await message.author.createDM()
-        dm.send(`Clotho has received your candidate "${newCandidate}"!`)
-    }
-
-    const duplicateSubmissions = Object.keys(poll.candidates)
-        .filter(user => user !== message.author.username)
-        .filter(user => poll.candidates[user].candidate.toLowerCase() === newCandidate.toLowerCase())
-
-    if (duplicateSubmissions.length) {
-        message.reply('Actually, this candidate has already been submitted by another user. Please submit again!')
-
-        const dm = await message.author.createDM()
-        dm.send(`Unfortunately, your proposed candidate "${newCandidate}" has already been submitted. Please submit a different candidate.`)
+    try {
+        await validate(poll, newCandidate)
+    } catch (error) {
+        message.reply(`ERROR: ${error.message}`)
         return
     }
 
+    if (username in poll.candidates) {
+        message.reply("Clotho has received another submission from you! It will replace your previous submission.")
+        await directMessage(author, `Clotho received another submission from you!\nYour previous candidate, "${poll.candidates[username].candidate}", will be replaced with your new submission, "${newCandidate}".`)
+    } else {
+        message.reply('your submission has been saved!')
+        await directMessage(author, `Clotho has received your candidate "${newCandidate}"!`)
+    }
+
     poll.candidates[username] = {
-        user: message.author,
+        user: author,
         candidate: newCandidate,
     }
 }
