0dc3e824e3e2d5bfe4594c8626f5edec84d7fd0a
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-11-19 11:47:23 -0700

    Wrap command panel in try/catch that sends a message to channel

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const config = require('./config.json')
-const actions  = require('./actions')
+const actions = require('./actions')
 
 const client = new Discord.Client()
 const prefix = '!!'
@@ -25,34 +25,39 @@ client.on('message', async message => {
         message.delete()
     }
 
-    switch (command) {
-        case 'ping':
-            await runCommand('Ping', () => actions.ping(message))
-            break
+    try {
+        switch (command) {
+            case 'ping':
+                await runCommand('Ping', () => actions.ping(message))
+                break
 
-        case 'startpoll':
-            await runCommand('Start Poll', () => actions.startPoll(poll, message, args))
-            break
+            case 'startpoll':
+                await runCommand('Start Poll', () => actions.startPoll(poll, message, args))
+                break
 
-        case 'submit':
-            await runCommand('Submit', () => actions.submit(poll, message, args))
-            break
+            case 'submit':
+                await runCommand('Submit', () => actions.submit(poll, message, args))
+                break
 
-        case 'closepoll':
-            await runCommand('Close Poll', () => actions.closePoll(poll, message))
-            break
+            case 'closepoll':
+                await runCommand('Close Poll', () => actions.closePoll(poll, message))
+                break
 
-        case 'declare':
-            await runCommand('Declare Victor', () => actions.declareVictor(poll, message))
-            break
+            case 'declare':
+                await runCommand('Declare Victor', () => actions.declareVictor(poll, message))
+                break
 
-        case 'help':
-            await runCommand('Help', () => actions.help(message))
-            break
+            case 'help':
+                await runCommand('Help', () => actions.help(message))
+                break
 
-        default:
-            message.reply(`"${prefix}${command}" is not a recognizable command! Please use "!!help" to get a list of valid commands.`)
-            break
+            default:
+                message.reply(`"${prefix}${command}" is not a recognizable command! Please use "!!help" to get a list of valid commands.`)
+                break
+        }
+    } catch (error) {
+        console.error(`Unhandled error - ${error.message}`)
+        message.channel.send(`Clotho experienced an error while processing command "${prefix}${command}": ${error.message}`)
     }
 })
 
