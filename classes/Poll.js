0f7077a7107199d857597bd07150247ad4ed97e6
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-11-19 21:26:02 -0700

    Create Poll class

---
+++ classes/Poll.js
@@ -0,0 +1,37 @@
+class Poll {
+    #title = ''
+    #strawPollId = ''
+    #candidates = {}
+
+    get title() {
+        return this.#title
+    }
+
+    set title(title) {
+        this.#title = title
+    }
+
+    get strawPollId() {
+        return this.#strawPollId
+    }
+
+    set strawPollId(pollId) {
+        this.#strawPollId = pollId
+    }
+
+    get candidates() {
+        return this.#candidates
+    }
+
+    addCandidate(username, candidate) {
+        this.#candidates[username] = candidate
+    }
+
+    resetPoll() {
+        this.#title = ''
+        this.#strawPollId = ''
+        this.#candidates = {}
+    }
+}
+
+module.exports = Poll
