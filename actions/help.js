3909ffe658d62a56f6b6ba3e3c44f2a92c64e473
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-12-25 01:11:35 -0700

    Update help text to include reset action

--- actions/help.js
+++ actions/help.js
@@ -8,6 +8,7 @@ Here's a list of the valid commands for Clotho!
 **!!submit [candidate for poll]** - Submit your candidate for the current poll. If you have already submitted a candidate, using this command again will replace your submission.
 **!!closepoll** - Stop taking submissions for the current poll and create a StrawPoll with the given candidates.
 **!!declare** - Declares a victor of the StrawPoll and erases the values in the poll object.
+**!!reset** - Resets all poll information to nothing. You have a clean slate!
 
 The typical order for using these commands is the way they are listed above (with the exception of the **!!ping** command).
 `
